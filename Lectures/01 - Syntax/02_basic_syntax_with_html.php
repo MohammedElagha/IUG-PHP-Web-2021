eecf9412d894e9becdf62229c6eae5a37f82733c
Mohammed Agha <dev.magha@gmail.com>
2021-02-26 20:35:31 +0200

    PHP syntax

---
+++ Lectures/01 - Syntax/02_basic_syntax_with_html.php
@@ -0,0 +1,17 @@
+<?php
+
+echo "Hello World!";
+
+?>
+
+<html>
+	<body>
+		<h1>Hello H1</h1>
+	</body>
+</html>
+
+<?php
+
+echo "Hello World!";
+
+?>
---
+++ Lectures/01 - Syntax/03_php_within_html.php
@@ -0,0 +1,14 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+
+	<?php echo "<h1>string</h1>"; ?>
+
+	<h1><?php echo "Hello World 2!"; ?></h1>
+
+</body>
+</html>
