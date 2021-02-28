3437350781ae28905da36f818d9d1b5492f41ddf
Mohammed Agha <dev.magha@gmail.com>
2021-02-28 13:46:13 +0200

    Increment and Decrement Operators

---
+++ Lectures/06 - Increment and Decrement Operators/01_increment_and_decrement_operators.php
@@ -0,0 +1,20 @@
+<?php
+
+$var = 5;
+
+# normal
+# $var = $var + 1;
+
+# assignment operators
+# $var += 1;
+
+# post-icrement and pre-increment as the same
+$var++;
+++$var;
+
+# post-icrement and pre-increment not as the same
+$new_var = ++$var;	# new_var = 6
+$new_var = $var++;	# new_var = 5
+
+
+?>
