93b8ea0540e5796fde72b8e847e6576933ea1ee6
Mohammed Agha <dev.magha@gmail.com>
2021-02-28 13:33:27 +0200

    assignment operators

---
+++ Lectures/05 - Assignment Operators/01_assignment_operators.php
@@ -0,0 +1,23 @@
+<?php
+
+# assign value to variable
+$var = 5;
+
+# assign variable to variable
+$var2 = $var;
+
+# add one to variable value
+$var = $var + 1;
+
+# add one to variable value (using assignment operator)
+$var += 1;
+
+# multiply variable value by 2 (using assignment operator)
+$var *= 2;
+
+/*
+allowed operators in assignment:
++ - * / %
+*/
+
+?>
